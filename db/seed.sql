4bd2d36eadcfdec49b64adb01144c51b51839e0b
Deepika Asthana <dasthana.24@gmail.com>
2018-06-08 00:02:03 -0600

    initial commit

---
+++ db/seed.sql
@@ -0,0 +1,9 @@
+
+INSERT INTO burgers (burger_name) VALUES ('Vegan BBQ Tofu Burger');
+INSERT INTO burgers (burger_name) VALUES ('Portobello and Peach Burgers');
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('McSpicy Chicken');
+INSERT INTO burgers (burger_name) VALUES ('bacon wrapped hamburger');
+
+
+-- select * from burgers
